2e78971db72cc7e460a80f1df2629eb4efecc38d
Семен Гузенков <guzenkovsk@gmail.com>
2024-02-11 23:11:56 +0700

    Homework#2 commit

---
+++ Homework#2/MainWindow.axaml.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Interactivity;
+using System.ComponentModel;
+using Avalonia.Controls.Shapes;
+
+
+
+namespace Homework_2
+{
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            rectangle = this.FindControl<Rectangle>("rectangle");
+        }
+
+        private void Button_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            rectangle.Fill = button.Background;
+        }
+
+    }
+}
